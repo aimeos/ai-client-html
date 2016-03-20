0286c466894682523fe43c5f473de6cd8181166e
unix <development@dragontale.de>
2016-03-20 22:12:07 +0100

    Add functionality for filter text reset, reuse sprite.
    Add functionality for filter text reset, reuse sprite close button as
    reset icon to the right in the search bar.
    
    Alternatively one may use the Shop index direct link. This is not
    implemented, yet it should be possible to manually press access the link.
    
    Note: The translation domain must be correct or "Non-recoverable error
    occurred" message appears. Exempli gratia at some point the key
    client/html was changed to just client. Still having client/html then
    leads to an error.
    
    Also hide the cross if text filter input field is empty.

--- client/html/themes/elegance/aimeos.js
+++ client/html/themes/elegance/aimeos.js
@@ -4,3 +4,41 @@
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Aimeos (aimeos.org), 2014
  */
+
+
+
+/**
+ * Hides or shows the catalog filter search reset button
+ * depending on if the input text field is empty or not.
+ */
+hide_or_show_catalog_filter_search_reset_button: function() {
+	if (this.value !== '')
+		this.nextElementSibling.style.display = 'inline-block';
+	else
+		this.nextElementSibling.style.display = 'none';
+}
+
+
+
+/**
+ * Resets the currently catalog filter search.
+ * In the simplest case the entered search term was not submitted
+ * yet, such that the field is just cleared.
+ * The major purpose of this field is to reset the search without
+ * having to manually empty the input field and submit it to show
+ * all items of all categories again.
+ */
+reset_catalog_filter_search: function() {
+//	var catalog_filter_search_input = $('.catalog-filter-search input:nth-of-type(1)');
+//	catalog_filter_search_input.val('');
+	this.previousElementSibling.val('');
+	this.parentNode.parentNode.submit();
+	this.style.display = 'none';
+}
+
+
+
+/* Register events: */
+$('.catalog-filter-search input:nth-of-type(1)').on('keyup', hide_or_show_catalog_filter_search_reset_button);
+$('.catalog-filter-search div:nth-of-type(1)').on('click', reset_catalog_filter_search);
+
