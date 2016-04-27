3f6ef7a2164c5335d436cb190aa980881701ad5c
Aimeos <aimeos@aimeos.org>
2016-04-27 12:11:51 +0200

    Moved HTML client to own extension

---
+++ client/html/templates/checkout/confirm/order-header-default.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license LGPLv3, http://www.gnu.org/licenses/lgpl.php
+ * @copyright Aimeos (aimeos.org), 2015
+ */
+
+?>
+<?php echo $this->get( 'orderHeader' ); ?>
---
+++ manifest.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+	'name' => 'ai-client-html',
+	'depends' => array(
+		'aimeos-core',
+	),
+	'include' => array(
+		'client/html/src',
+	),
+	'i18n' => array(
+		'client' => 'client/i18n',
+		'client/code' => 'client/i18n/code',
+		'client/country' => 'client/i18n/country',
+		'client/currency' => 'client/i18n/currency',
+		'client/language' => 'client/i18n/language',
+	),
+	'config' => array(
+		'client/html/config',
+	),
+	'custom' => array(
+		'client/html/templates' => array(
+			'client/html/templates',
+		),
+	),
+);
