bb73abcfee8071256c6e31892eb54e423f980e0c
Aimeos <aimeos@aimeos.org>
2016-09-26 14:55:31 +0200

    Streamlined comment HTML

--- client/html/templates/checkout/standard/summary-comment-body-default.php
+++ client/html/templates/checkout/standard/summary-comment-body-default.php
@@ -10,7 +10,8 @@ $enc = $this->encoder();
 ?>
 <?php $this->block()->start( 'checkout/standard/summary/comment' ); ?>
 <div class="checkout-standard-summary-comment container">
-	<h2 class="header"><?php echo $enc->html( $this->translate( 'client', 'Comment' ), $enc::TRUST ); ?></h2>
+	<h2><?php echo $enc->html( $this->translate( 'client', 'Comment' ), $enc::TRUST ); ?></h2>
+	<div class="header"><h3><?php echo $enc->html( $this->translate( 'client', 'Your comment' ), $enc::TRUST ); ?></h3></div>
 	<textarea class="comment-value" name="<?php echo $this->formparam( array( 'cs_comment' ) ); ?>"><?php echo $enc->html( $this->summaryComment ); ?></textarea>
 <?php echo $this->get( 'optionBody' ); ?>
 </div>
