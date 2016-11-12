6a7f62f3cceef84557b6cbdc4cb8fc271220f59c
Aimeos <aimeos@aimeos.org>
2016-11-12 16:58:48 +0100

    Removed unused templates for checkout order account/address parts

--- client/html/src/Client/Html/Checkout/Standard/Order/Account/Standard.php
+++ client/html/src/Client/Html/Checkout/Standard/Order/Account/Standard.php
@@ -74,32 +74,8 @@ class Standard
 		foreach( $this->getSubClients() as $subclient ) {
 			$html .= $subclient->setView( $view )->getBody( $uid, $tags, $expire );
 		}
-		$view->accountBody = $html;
 
-		/** client/html/checkout/standard/order/account/standard/template-body
-		 * Relative path to the HTML body template of the checkout standard order account client.
-		 *
-		 * The template file contains the HTML code and processing instructions
-		 * to generate the result shown in the body of the frontend. The
-		 * configuration string is the path to the template file relative
-		 * to the templates directory (usually in client/html/templates).
-		 *
-		 * You can overwrite the template file configuration in extensions and
-		 * provide alternative templates. These alternative templates should be
-		 * named like the default one but with the string "standard" replaced by
-		 * an unique name. You may use the name of your project for this. If
-		 * you've implemented an alternative client class as well, "standard"
-		 * should be replaced by the name of the new class.
-		 *
-		 * @param string Relative path to the template creating code for the HTML page body
-		 * @since 2015.09
-		 * @category Developer
-		 * @see client/html/checkout/standard/order/account/standard/template-header
-		 */
-		$tplconf = 'client/html/checkout/standard/order/account/standard/template-body';
-		$default = 'checkout/standard/order-account-body-default.php';
-
-		return $view->render( $view->config( $tplconf, $default ) );
+		return $html;
 	}
 
 
