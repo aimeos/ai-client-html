a7401ada93ddfea118f0fab7620165b03c845370
Aimeos <aimeos@aimeos.org>
2016-11-12 17:58:29 +0100

    Fixed key for template paths

--- controller/jobs/src/Controller/Jobs/Customer/Email/Account/Standard.php
+++ controller/jobs/src/Controller/Jobs/Customer/Email/Account/Standard.php
@@ -94,7 +94,7 @@ class Standard
 	{
 		if( !isset( $this->client ) )
 		{
-			$templatePaths = $this->getAimeos()->getCustomPaths( 'client/html' );
+			$templatePaths = $this->getAimeos()->getCustomPaths( 'client/html/templates' );
 			$this->client = \Aimeos\Client\Html\Email\Account\Factory::createClient( $context, $templatePaths );
 		}
 
