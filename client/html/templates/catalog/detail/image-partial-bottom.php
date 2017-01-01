7183901bbeb6f39b68b09687b278012ffd6e3954
Aimeos <aimeos@aimeos.org>
2017-01-01 14:57:42 +0100

    Replaced carousel and image zoom by Slick slider and PhotoSwipe

--- client/html/templates/catalog/detail/image-partial-bottom.php
+++ client/html/templates/catalog/detail/image-partial-bottom.php
@@ -23,75 +23,9 @@ $getVariantData = function( $mediaId, array $mediaItems ) use ( $enc )
 };
 
 
-/** client/html/catalog/detail/url/target
- * Destination of the URL where the controller specified in the URL is known
- *
- * The destination can be a page ID like in a content management system or the
- * module of a software development framework. This "target" must contain or know
- * the controller that should be called by the generated URL.
- *
- * @param string Destination of the URL
- * @since 2014.03
- * @category Developer
- * @see client/html/catalog/detail/url/controller
- * @see client/html/catalog/detail/url/action
- * @see client/html/catalog/detail/url/config
- */
 $detailTarget = $this->config( 'client/html/catalog/detail/url/target' );
-
-/** client/html/catalog/detail/url/controller
- * Name of the controller whose action should be called
- *
- * In Model-View-Controller (MVC) applications, the controller contains the methods
- * that create parts of the output displayed in the generated HTML page. Controller
- * names are usually alpha-numeric.
- *
- * @param string Name of the controller
- * @since 2014.03
- * @category Developer
- * @see client/html/catalog/detail/url/target
- * @see client/html/catalog/detail/url/action
- * @see client/html/catalog/detail/url/config
- */
 $detailController = $this->config( 'client/html/catalog/detail/url/controller', 'catalog' );
-
-/** client/html/catalog/detail/url/action
- * Name of the action that should create the output
- *
- * In Model-View-Controller (MVC) applications, actions are the methods of a
- * controller that create parts of the output displayed in the generated HTML page.
- * Action names are usually alpha-numeric.
- *
- * @param string Name of the action
- * @since 2014.03
- * @category Developer
- * @see client/html/catalog/detail/url/target
- * @see client/html/catalog/detail/url/controller
- * @see client/html/catalog/detail/url/config
- */
 $detailAction = $this->config( 'client/html/catalog/detail/url/action', 'detail' );
-
-/** client/html/catalog/detail/url/config
- * Associative list of configuration options used for generating the URL
- *
- * You can specify additional options as key/value pairs used when generating
- * the URLs, like
- *
- *  client/html/<clientname>/url/config = array( 'absoluteUri' => true )
- *
- * The available key/value pairs depend on the application that embeds the e-commerce
- * framework. This is because the infrastructure of the application is used for
- * generating the URLs. The full list of available config options is referenced
- * in the "see also" section of this page.
- *
- * @param string Associative list of configuration options
- * @since 2014.03
- * @category Developer
- * @see client/html/catalog/detail/url/target
- * @see client/html/catalog/detail/url/controller
- * @see client/html/catalog/detail/url/action
- * @see client/html/url/config
- */
 $detailConfig = $this->config( 'client/html/catalog/detail/url/config', array() );
 
 $url = $enc->attr( $this->url( $detailTarget, $detailController, $detailAction, $this->get( 'params', array() ), array(), $detailConfig ) );
@@ -99,44 +33,103 @@ $media = $this->get( 'mediaItems', array() );
 
 
 ?>
-<div class="catalog-detail-image" data-dir="vertical">
+<div class="catalog-detail-image">
 	<?php $mediaItems = $this->product->getRefItems( 'media', 'default', 'default' ); ?>
 
-	<div class="image-thumbs">
-		<a class="prev-thumbs disabled"></a>
-		<div class="thumbs">
-			<?php if( count( $mediaItems ) > 1 ) : $class = 'item selected'; ?>
+	<?php if( count( $mediaItems ) > 1 ) : $class = 'item selected'; ?>
+		<div class="image-thumbs thumbs-vertical" data-slick='{"slidesToShow": 4, "slidesToScroll": 4, "vertical": true, "verticalSwiping": true}'>
+			<button type="button" class="slick-prev"><?php echo $enc->html( $this->translate( 'client', 'Previous' ) ); ?></button>
+			<div class="thumbs">
+
 				<?php foreach( $mediaItems as $id => $mediaItem ) : ?>
 					<?php $previewUrl = $enc->attr( $this->content( $mediaItem->getPreview() ) ); ?>
 
-					<a class="<?php echo $class; ?>"
-						style="background-image: url('<?php echo $previewUrl; ?>')"
+					<a class="<?php echo $class; ?>" style="background-image: url('<?php echo $previewUrl; ?>')"
 						href="<?php echo $url . '#image-' . $enc->attr( $id ); ?>"
 					></a>
 
 					<?php $class = 'item'; ?>
 				<?php endforeach; ?>
-			<?php endif; ?>
-		</div>
-		<a class="next-thumbs disabled"></a>
-	</div><!--
 
-	--><div class="image-single">
-		<div class="carousel">
-			<?php foreach( $mediaItems as $id => $mediaItem ) : ?>
-				<?php $mediaUrl = $enc->attr( $this->content( $mediaItem->getUrl() ) ); ?>
+			</div>
+			<button type="button" class="slick-next"><?php echo $enc->html( $this->translate( 'client', 'Next' ) ); ?></button>
+		</div><!--
+	--><?php endif; ?><!--
 
-				<div id="image-<?php echo $enc->attr( $id ); ?>" class="item"
-					style="background-image: url('<?php echo $mediaUrl; ?>')"
-					data-image="<?php echo $mediaUrl; ?>"
-					data-zoom-image="<?php echo $mediaUrl; ?>"
-					<?php echo $getVariantData( $id, $media ); ?>
-					itemscope="" itemtype="http://schema.org/ImageObject">
-					<meta itemprop="contentUrl" content="<?php echo $mediaUrl; ?>" />
-				</div>
+	--><div class="image-single" data-pswp="{bgOpacity: 0.75, shareButtons: false}">
 
-			<?php endforeach; ?>
-		</div>
+		<?php foreach( $mediaItems as $id => $mediaItem ) : ?>
+			<?php $mediaUrl = $enc->attr( $this->content( $mediaItem->getUrl() ) ); ?>
+			<?php $previewUrl = $enc->attr( $this->content( $mediaItem->getPreview() ) ); ?>
+
+			<figure id="image-<?php echo $enc->attr( $id ); ?>"
+				class="item" style="background-image: url('<?php echo $mediaUrl; ?>')"
+				itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"
+				data-image="<?php echo $previewUrl; ?>"
+				<?php echo $getVariantData( $id, $media ); ?> >
+				<a href="<?php echo $enc->attr( $mediaUrl ); ?>" itemprop="contentUrl"></a>
+				<figcaption itemprop="caption description"><?php echo $enc->html( $mediaItem->getName() ); ?></figcaption>
+			</figure>
+
+		<?php endforeach; ?>
+
+	</div>
+
+
+	<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+	    <div class="pswp__bg"></div>
+	    <div class="pswp__scroll-wrap">
+
+	        <!-- Container that holds slides. Don't modify these 3 pswp__item elements, data is added later on. -->
+	        <div class="pswp__container">
+	            <div class="pswp__item"></div>
+	            <div class="pswp__item"></div>
+	            <div class="pswp__item"></div>
+	        </div>
+
+	        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+	        <div class="pswp__ui pswp__ui--hidden">
+	            <div class="pswp__top-bar">
+
+	                <div class="pswp__counter"></div>
+
+	                <button class="pswp__button pswp__button--close"
+	                	title="<?php echo $enc->attr( $this->translate( 'client', 'Close' ) ); ?>">
+	                </button>
+	                <!-- button class="pswp__button pswp__button--share"
+	                	title="<?php echo $enc->attr( $this->translate( 'client', 'Share' ) ); ?>">
+	                </button -->
+	                <button class="pswp__button pswp__button--fs"
+	                	title="<?php echo $enc->attr( $this->translate( 'client', 'Toggle fullscreen' ) ); ?>">
+	                </button>
+	                <button class="pswp__button pswp__button--zoom"
+	                	title="<?php echo $enc->attr( $this->translate( 'client', 'Zoom in/out' ) ); ?>">
+	                </button>
+
+	                <div class="pswp__preloader">
+	                    <div class="pswp__preloader__icn">
+	                      <div class="pswp__preloader__cut">
+	                        <div class="pswp__preloader__donut"></div>
+	                      </div>
+	                    </div>
+	                </div>
+	            </div>
+
+	            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+	                <div class="pswp__share-tooltip"></div>
+	            </div>
+
+	            <button class="pswp__button pswp__button--arrow--left"
+                	title="<?php echo $enc->attr( $this->translate( 'client', 'Previous' ) ); ?>">
+	            </button>
+	            <button class="pswp__button pswp__button--arrow--right"
+                	title="<?php echo $enc->attr( $this->translate( 'client', 'Next' ) ); ?>">
+	            </button>
+
+	            <div class="pswp__caption"><div class="pswp__caption__center"></div></div>
+
+	        </div>
+	    </div>
 	</div>
 
 </div>
