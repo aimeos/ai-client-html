c805c7cf0fc7610d40aed935a4a165c82ea63c81
Aimeos <aimeos@aimeos.org>
2017-02-13 15:45:17 +0100

    Fixed used currency parameter name in tests

--- client/html/tests/Client/Html/Locale/Select/Currency/StandardTest.php
+++ client/html/tests/Client/Html/Locale/Select/Currency/StandardTest.php
@@ -36,10 +36,10 @@ class StandardTest extends \PHPUnit_Framework_TestCase
 		$view->selectLanguageId = 'de';
 		$view->selectMap = array(
 			'de' => array(
-				'EUR' => array( 'locale' => 'de', 'currencyid' => 'EUR' ),
-				'CHF' => array( 'locale' => 'de', 'currencyid' => 'CHF' ),
+				'EUR' => array( 'locale' => 'de', 'currency' => 'EUR' ),
+				'CHF' => array( 'locale' => 'de', 'currency' => 'CHF' ),
 			),
-			'en' => array( 'USD' => array( 'locale' => 'en', 'currencyid' => 'USD' ) ),
+			'en' => array( 'USD' => array( 'locale' => 'en', 'currency' => 'USD' ) ),
 		);
 
 		$request = $this->getMockBuilder( '\Psr\Http\Message\ServerRequestInterface' )->getMock();
