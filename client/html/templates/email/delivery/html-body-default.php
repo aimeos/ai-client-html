9fc1507259f22b3d01b92c9fa1175fb4de30e34f
Peter Acs <pxp121@gmail.com>
2017-05-29 17:14:54 +0900

    Hotfix/email template (#32)
    move style from body to head in e-mails

--- client/html/templates/email/delivery/html-body-default.php
+++ client/html/templates/email/delivery/html-body-default.php
@@ -18,50 +18,49 @@ $title = $this->translate( 'client', 'E-mail notification' );
 		<title><?= $enc->html( $title, $enc::TRUST ); ?></title>
 		<meta name="application-name" content="Aimeos" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style type="text/css">
+            html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+            a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp,
+            small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li,
+            fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td,
+            article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup,
+            menu, nav, output, ruby, section, summary, time, mark, audio, video {
+                margin: 0;
+                padding: 0;
+                border: 0;
+                font-size: 100%;
+                font: inherit;
+                vertical-align: baseline;
+            }
+            /* HTML5 display-role reset for older browsers */
+            article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+                display: block;
+            }
+
+            .aimeos .content-block {
+                margin: 1.5em 2%;
+                width: 96%;
+            }
+            .aimeos .logo {
+                margin: 1.5em 2%;
+                margin-top: 0;
+                float: right;
+            }
+            .aimeos .common-summary {
+                clear: both;
+            }
+            .account-detail h2 {
+                font-weight: bold;
+            }
+            .account-detail .name:after {
+                content: ":";
+            }
+
+            <?= $this->get( 'htmlCss' ); ?>
+
+        </style>
 	</head>
 	<body>
-		<style type="text/css">
-			html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-			a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp,
-			small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li,
-			fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td,
-			article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup,
-			menu, nav, output, ruby, section, summary, time, mark, audio, video {
-				margin: 0;
-				padding: 0;
-				border: 0;
-				font-size: 100%;
-				font: inherit;
-				vertical-align: baseline;
-			}
-			/* HTML5 display-role reset for older browsers */
-			article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
-				display: block;
-			}
-
-			.aimeos .content-block {
-				margin: 1.5em 2%;
-				width: 96%;
-			}
-			.aimeos .logo {
-				margin: 1.5em 2%;
-				margin-top: 0;
-				float: right;
-			}
-			.aimeos .common-summary {
-				clear: both;
-			}
-			.account-detail h2 {
-				font-weight: bold;
-			}
-			.account-detail .name:after {
-				content: ":";
-			}
-
-			<?= $this->get( 'htmlCss' ); ?>
-
-		</style>
-
 		<div class="aimeos">
 
 			<?php if( isset( $this->htmlLogo ) ) : ?>
