8d23e5ac5333c4bc0d3fffca2ea100161bb0afd9
Aimeos <aimeos@aimeos.org>
2018-04-12 12:29:22 +0200

    Added documentation for new catalog components derived from catalog filter

--- client/html/src/Client/Html/Catalog/Tree/Standard.php
+++ client/html/src/Client/Html/Catalog/Tree/Standard.php
@@ -59,6 +59,93 @@ class Standard
 
 
 	/**
+	 * Returns the sub-client given by its name.
+	 *
+	 * @param string $type Name of the client type
+	 * @param string|null $name Name of the sub-client (Default if null)
+	 * @return \Aimeos\Client\Html\Iface Sub-client object
+	 */
+	public function getSubClient( $type, $name = null )
+	{
+		/** client/html/catalog/tree/decorators/excludes
+		 * Excludes decorators added by the "common" option from the catalog filter tree html client
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to remove a decorator added via
+		 * "client/html/common/decorators/default" before they are wrapped
+		 * around the html client.
+		 *
+		 *  client/html/catalog/tree/decorators/excludes = array( 'decorator1' )
+		 *
+		 * This would remove the decorator named "decorator1" from the list of
+		 * common decorators ("\Aimeos\Client\Html\Common\Decorator\*") added via
+		 * "client/html/common/decorators/default" to the html client.
+		 *
+		 * @param array List of decorator names
+		 * @since 2018.04
+		 * @category Developer
+		 * @see client/html/common/decorators/default
+		 * @see client/html/catalog/tree/decorators/global
+		 * @see client/html/catalog/tree/decorators/local
+		 */
+
+		/** client/html/catalog/tree/decorators/global
+		 * Adds a list of globally available decorators only to the catalog filter tree html client
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap global decorators
+		 * ("\Aimeos\Client\Html\Common\Decorator\*") around the html client.
+		 *
+		 *  client/html/catalog/tree/decorators/global = array( 'decorator1' )
+		 *
+		 * This would add the decorator named "decorator1" defined by
+		 * "\Aimeos\Client\Html\Common\Decorator\Decorator1" only to the html client.
+		 *
+		 * @param array List of decorator names
+		 * @since 2018.04
+		 * @category Developer
+		 * @see client/html/common/decorators/default
+		 * @see client/html/catalog/tree/decorators/excludes
+		 * @see client/html/catalog/tree/decorators/local
+		 */
+
+		/** client/html/catalog/tree/decorators/local
+		 * Adds a list of local decorators only to the catalog filter tree html client
+		 *
+		 * Decorators extend the functionality of a class by adding new aspects
+		 * (e.g. log what is currently done), executing the methods of the underlying
+		 * class only in certain conditions (e.g. only for logged in users) or
+		 * modify what is returned to the caller.
+		 *
+		 * This option allows you to wrap local decorators
+		 * ("\Aimeos\Client\Html\Catalog\Decorator\*") around the html client.
+		 *
+		 *  client/html/catalog/tree/decorators/local = array( 'decorator2' )
+		 *
+		 * This would add the decorator named "decorator2" defined by
+		 * "\Aimeos\Client\Html\Catalog\Decorator\Decorator2" only to the html client.
+		 *
+		 * @param array List of decorator names
+		 * @since 2018.04
+		 * @category Developer
+		 * @see client/html/common/decorators/default
+		 * @see client/html/catalog/tree/decorators/excludes
+		 * @see client/html/catalog/tree/decorators/global
+		 */
+
+		return parent::getSubClient( $type, $name );
+	}
+
+
+	/**
 	 * Returns the names of the subpart clients
 	 *
 	 * @return array List of client names
