db79538bad46f316900b9f04909e90d30c69a9be
Aimeos <aimeos@aimeos.org>
2018-09-29 15:39:48 +0200

    Moved attribute filter to the end The selected filters will break the horizontal layout otherwise

--- client/html/templates/catalog/filter/body-standard.php
+++ client/html/templates/catalog/filter/body-standard.php
@@ -121,8 +121,8 @@ $enc = $this->encoder();
 
 			<?= $this->block()->get( 'catalog/filter/search' ); ?>
 			<?= $this->block()->get( 'catalog/filter/tree' ); ?>
-			<?= $this->block()->get( 'catalog/filter/attribute' ); ?>
 			<?= $this->block()->get( 'catalog/filter/supplier' ); ?>
+			<?= $this->block()->get( 'catalog/filter/attribute' ); ?>
 
 		</form>
 	</nav>
