36c04d6767b71bc31c34cdb6cf1b8cf2b9a2ac04
Aimeos <aimeos@aimeos.org>
2018-12-23 11:42:27 +0100

    Implemented simplified factory for HTML clients

--- client/html/src/Client/Html/Factory.php
+++ client/html/src/Client/Html/Factory.php
@@ -16,8 +16,9 @@ namespace Aimeos\Client\Html;
  *
  * @package Client
  * @subpackage Html
+ * @deprecated Use Html class instead
  */
-class Factory
+class Factory extends \Aimeos\Client\Html
 {
 	/**
 	 * Creates a new client object.
@@ -30,36 +31,6 @@ class Factory
 	 */
 	public static function createClient( \Aimeos\MShop\Context\Item\Iface $context, $type, $name = null )
 	{
-		if( empty( $type ) ) {
-			throw new \Aimeos\Client\Html\Exception( sprintf( 'Client HTML type is empty' ) );
-		}
-
-		$parts = explode( '/', $type );
-
-		if( count( $parts ) !== 2 ) {
-			throw new \Aimeos\Client\Html\Exception( sprintf( 'Client type "%1$s" must consist of two parts separated by "/"', $type ) );
-		}
-
-		foreach( $parts as $part )
-		{
-			if( ctype_alnum( $part ) === false ) {
-				throw new \Aimeos\Client\Html\Exception( sprintf( 'Invalid characters in client name "%1$s" in "%2$s"', $part, $type ) );
-			}
-		}
-
-		$factory = '\\Aimeos\\Client\\Html\\' . ucwords( $parts[0] ) . '\\' . ucwords( $parts[1] ) . '\\Factory';
-
-		if( class_exists( $factory ) === false ) {
-			throw new \Aimeos\Client\Html\Exception( sprintf( 'Class "%1$s" not available', $factory ) );
-		}
-
-		$client = @call_user_func_array( array( $factory, 'createClient' ), array( $context, $name ) );
-
-		if( $client === false ) {
-			throw new \Aimeos\Client\Html\Exception( sprintf( 'Invalid factory "%1$s"', $factory ) );
-		}
-
-		return $client;
+		return self::create( $context, $type, $name );
 	}
-
 }
