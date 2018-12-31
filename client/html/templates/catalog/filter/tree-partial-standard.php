028f7b7f7dc9c2dde9cf2113c7600c82f9a98fdb
Aimeos <aimeos@aimeos.org>
2018-12-31 16:01:21 +0100

    Added separate route for product categories

--- client/html/templates/catalog/filter/tree-partial-standard.php
+++ client/html/templates/catalog/filter/tree-partial-standard.php
@@ -10,10 +10,78 @@ $level = $this->get( 'level', 0 );
 $path = $this->get( 'path', [] );
 $params = $this->get( 'params', [] );
 
-$target = $this->config( 'client/html/catalog/lists/url/target' );
-$controller = $this->config( 'client/html/catalog/lists/url/controller', 'catalog' );
-$action = $this->config( 'client/html/catalog/lists/url/action', 'list' );
-$config = $this->config( 'client/html/catalog/lists/url/config', [] );
+
+/** client/html/catalog/tree/url/target
+ * Destination of the URL where the controller specified in the URL is known
+ *
+ * The destination can be a page ID like in a content management system or the
+ * module of a software development framework. This "target" must contain or know
+ * the controller that should be called by the generated URL.
+ *
+ * @param string Destination of the URL
+ * @since 2019.01
+ * @category Developer
+ * @see client/html/catalog/tree/url/controller
+ * @see client/html/catalog/tree/url/action
+ * @see client/html/catalog/tree/url/config
+ */
+$target = $this->config( 'client/html/catalog/tree/url/target' );
+
+/** client/html/catalog/tree/url/controller
+ * Name of the controller whose action should be called
+ *
+ * In Model-View-Controller (MVC) applications, the controller contains the methods
+ * that create parts of the output displayed in the generated HTML page. Controller
+ * names are usually alpha-numeric.
+ *
+ * @param string Name of the controller
+ * @since 2019.01
+ * @category Developer
+ * @see client/html/catalog/tree/url/target
+ * @see client/html/catalog/tree/url/action
+ * @see client/html/catalog/tree/url/config
+ */
+$controller = $this->config( 'client/html/catalog/tree/url/controller', 'catalog' );
+
+/** client/html/catalog/tree/url/action
+ * Name of the action that should create the output
+ *
+ * In Model-View-Controller (MVC) applications, actions are the methods of a
+ * controller that create parts of the output displayed in the generated HTML page.
+ * Action names are usually alpha-numeric.
+ *
+ * @param string Name of the action
+ * @since 2019.01
+ * @category Developer
+ * @see client/html/catalog/tree/url/target
+ * @see client/html/catalog/tree/url/controller
+ * @see client/html/catalog/tree/url/config
+ */
+$action = $this->config( 'client/html/catalog/tree/url/action', 'list' );
+
+/** client/html/catalog/tree/url/config
+ * Associative list of configuration options used for generating the URL
+ *
+ * You can specify additional options as key/value pairs used when generating
+ * the URLs, like
+ *
+ *  client/html/<clientname>/url/config = array( 'absoluteUri' => true )
+ *
+ * The available key/value pairs depend on the application that embeds the e-commerce
+ * framework. This is because the infrastructure of the application is used for
+ * generating the URLs. The full list of available config options is referenced
+ * in the "see also" section of this page.
+ *
+ * @param string Associative list of configuration options
+ * @since 2019.01
+ * @category Developer
+ * @see client/html/catalog/tree/url/target
+ * @see client/html/catalog/tree/url/controller
+ * @see client/html/catalog/tree/url/action
+ * @see client/html/url/config
+ */
+$config = $this->config( 'client/html/catalog/tree/url/config', [] );
+
 
 /** client/html/common/partials/media
  * Relative path to the media partial template file
