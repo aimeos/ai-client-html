2f03a312701c1058b5a154b1280c539c7fbe99cf
Aimeos <aimeos@aimeos.org>
2019-06-14 18:41:04 +0200

    Fixed copyright

--- client/html/templates/catalog/product/header-standard.php
+++ client/html/templates/catalog/product/header-standard.php
@@ -2,31 +2,13 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2012
- * @copyright Aimeos (aimeos.org), 2015-2018
+ * @copyright Aimeos (aimeos.org), 2019
  */
-
-/** client/html/catalog/lists/metatags
- * Adds the title, meta and link tags to the HTML header
- *
- * By default, each instance of the catalog list component adds some HTML meta
- * tags to the page head section, like page title, meta keywords and description
- * as well as some link tags to support browser navigation. If several instances
- * are placed on one page, this leads to adding several title and meta tags used
- * by search engine. This setting enables you to suppress these tags in the page
- * header and maybe add your own to the page manually.
- *
- * @param boolean True to display the meta tags, false to hide it
- * @since 2017.01
- * @category Developer
- * @category User
- * @see client/html/catalog/detail/metatags
- */
-
 
 $enc = $this->encoder();
-?>
 
+
+?>
 <?php if( isset( $this->itemsStockUrl ) ) : ?>
 	<?php foreach( (array) $this->itemsStockUrl as $url ) : ?>
 		<script type="text/javascript" defer="defer" src="<?= $enc->attr( $url ); ?>"></script>
