3d4e984b0b8b8c1fd993523a1fc4427663bee70d
Aimeos <aimeos@aimeos.org>
2020-06-08 09:55:32 +0200

    Use Locale number view helper

--- controller/jobs/src/Controller/Jobs/Customer/Email/Watch/Standard.php
+++ controller/jobs/src/Controller/Jobs/Customer/Email/Watch/Standard.php
@@ -350,7 +350,7 @@ class Standard
 		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $view, $params );
 		$view->addHelper( 'param', $helper );
 
-		$helper = new \Aimeos\MW\View\Helper\Number\Standard( $view, $context->getLocale()->getLanguageId() );
+		$helper = new \Aimeos\MW\View\Helper\Number\Locale( $view, $context->getLocale()->getLanguageId() );
 		$view->addHelper( 'number', $helper );
 
 		$helper = new \Aimeos\MW\View\Helper\Translate\Standard( $view, $context->getI18n( $address->getLanguageId() ) );
