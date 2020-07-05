b07592b987319f7cb2814f66cd78f4f19ddf6211
Aimeos <aimeos@aimeos.org>
2020-07-05 17:46:34 +0200

    Added PDF subpart to payment e-mail

--- client/html/src/Client/Html/Email/Payment/Standard.php
+++ client/html/src/Client/Html/Email/Payment/Standard.php
@@ -78,7 +78,7 @@ class Standard
 	 * @since 2014.03
 	 * @category Developer
 	 */
-	private $subPartNames = array( 'text', 'html' );
+	private $subPartNames = array( 'text', 'html', 'pdf' );
 
 
 	/**
