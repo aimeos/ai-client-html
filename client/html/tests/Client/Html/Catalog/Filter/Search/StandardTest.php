b820deb97b2889f34b79edc63b7eed75351ddc31
Aimeos <aimeos@aimeos.org>
2020-10-15 10:33:50 +0200

    Adapt catalog search filter tests

--- client/html/tests/Client/Html/Catalog/Filter/Search/StandardTest.php
+++ client/html/tests/Client/Html/Catalog/Filter/Search/StandardTest.php
@@ -31,7 +31,7 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	public function testGetBody()
 	{
 		$output = $this->object->getBody();
-		$this->assertStringStartsWith( '<section class="catalog-filter-search">', $output );
+		$this->assertStringStartsWith( '<section class="catalog-filter-search', $output );
 	}
 
 
