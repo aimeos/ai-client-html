4f781d27e5acc0f5e7a60cce575216bdb19cc85d
Aimeos <aimeos@aimeos.org>
2021-04-05 10:22:26 +0200

    Added more meta tags to catalog home header

--- client/html/templates/catalog/home/header-standard.php
+++ client/html/templates/catalog/home/header-standard.php
@@ -7,19 +7,36 @@
 
 $enc = $this->encoder();
 
+$target = $this->config( 'client/html/catalog/home/url/target' );
+$cntl = $this->config( 'client/html/catalog/home/url/controller', 'catalog' );
+$action = $this->config( 'client/html/catalog/home/url/action', 'home' );
+$config = $this->config( 'client/html/catalog/home/url/config', [] );
+
 
 ?>
 <?php if( isset( $this->homeTree ) ) : ?>
 
 	<title><?= $enc->html( strip_tags( $this->homeTree->getName() ) ) ?> | <?= $enc->html( $this->get( 'contextSiteLabel', 'Aimeos' ) ) ?></title>
 
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="<?= $enc->attr( $this->get( 'contextSiteLabel', 'Aimeos' ) ) ?>">
+	<meta property="og:title" content="<?= $enc->attr( strip_tags( $this->homeTree->getName() ) ) ?>" />
+	<meta property="og:url" content="<?= $enc->attr( $this->url( $target, $cntl, $action, [], $config + ['absoluteUri' => true] ) ) ?>" />
+
+	<?php foreach( $this->homeTree->getRefItems( 'media', 'default', 'default' ) as $mediaItem ) : ?>
+		<meta property="og:image" content="<?= $enc->attr( $this->content( $mediaItem->getUrl() ) ) ?>" />
+	<?php endforeach ?>
+
+	<?php foreach( $this->homeTree->getRefItems( 'text', 'meta-description', 'default' ) as $textItem ) : ?>
+		<meta property="og:description" content="<?= $enc->attr( $textItem->getContent() ) ?>" />
+		<meta name="description" content="<?= $enc->attr( strip_tags( $textItem->getContent() ) ); ?>" />
+	<?php endforeach ?>
+
 	<?php foreach( $this->homeTree->getRefItems( 'text', 'meta-keyword', 'default' ) as $textItem ) : ?>
 		<meta name="keywords" content="<?= $enc->attr( strip_tags( $textItem->getContent() ) ); ?>" />
 	<?php endforeach; ?>
 
-	<?php foreach( $this->homeTree->getRefItems( 'text', 'meta-description', 'default' ) as $textItem ) : ?>
-		<meta name="description" content="<?= $enc->attr( strip_tags( $textItem->getContent() ) ); ?>" />
-	<?php endforeach; ?>
+	<meta name="twitter:card" content="summary_large_image" />
 
 <?php else : ?>
 
