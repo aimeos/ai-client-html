0e109f5570ee1a29cd0f3551615cbae7b9115e6c
Marcus Förster <m.foerster@xerc.de>
2021-04-06 16:35:45 +0200

    force absolute URL @ og:image

--- client/html/templates/catalog/detail/header-standard.php
+++ client/html/templates/catalog/detail/header-standard.php
@@ -46,7 +46,7 @@ $detailFilter = array_flip( $this->config( 'client/html/catalog/detail/url/filte
 		<meta property="og:url" content="<?= $enc->attr( $this->url( $detailTarget, $detailController, $detailAction, $params, [], $detailConfig + ['absoluteUri' => true] ) ) ?>">
 
 		<?php foreach( $this->detailProductItem->getRefItems( 'media', 'default', 'default' ) as $mediaItem ) : ?>
-			<meta property="og:image" content="<?= $enc->attr( $this->content( $mediaItem->getPreview( true ) ) ) ?>">
+			<meta property="og:image" content="<?= $enc->attr( asset( $mediaItem->getPreview( true ) ) ) ?>">
 		<?php endforeach ?>
 
 		<?php foreach( $this->detailProductItem->getRefItems( 'text', 'meta-description', 'default' ) as $textItem ) : ?>
