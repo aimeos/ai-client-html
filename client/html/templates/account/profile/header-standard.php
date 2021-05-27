d31c534f304c5377ce88fb0d2b9c4b5f21d1532d
Marcus Förster <git@xerc.de>
2021-05-27 15:31:08 +0200

    [ADD] <title> @ account/profile (#131)

--- client/html/templates/account/profile/header-standard.php
+++ client/html/templates/account/profile/header-standard.php
@@ -5,5 +5,9 @@
  * @copyright Aimeos (aimeos.org), 2016-2021
  */
 
+$enc = $this->encoder();
+
 ?>
+<title><?= $this->translate( 'client', 'Profile' ) ?> | <?= $enc->html( $this->get( 'contextSiteLabel', 'Aimeos' ) ) ?></title>
+
 <?= $this->get( 'profileHeader' );
