797036749f040c533d0ad925a906eff332e98766
Aimeos <aimeos@aimeos.org>
2021-05-27 08:20:48 +0200

    Better site icon handling

--- client/html/templates/catalog/home/header-standard.php
+++ client/html/templates/catalog/home/header-standard.php
@@ -23,8 +23,8 @@ $config = $this->config( 'client/html/catalog/home/url/config', [] );
 	<meta property="og:title" content="<?= $enc->attr( strip_tags( $this->homeTree->getName() ) ) ?>">
 	<meta property="og:url" content="<?= $enc->attr( $this->url( $target, $cntl, $action, [], [], $config + ['absoluteUri' => true] ) ) ?>">
 
-	<?php if( isset( $this->contextSiteIcon ) ) : ?>
-		<meta property="og:image" content="<?= $enc->attr( $this->content( $this->contextSiteIcon ) ) ?>">
+	<?php if( $icon = $this->get( 'contextSiteIcon' ) ) : ?>
+		<meta property="og:image" content="<?= $enc->attr( $this->content( $icon ) ) ?>">
 	<?php endif ?>
 
 	<?php foreach( $this->homeTree->getRefItems( 'text', 'meta-description', 'default' ) as $textItem ) : ?>
