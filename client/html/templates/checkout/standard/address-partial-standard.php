210fa529fe8a0c705133f61262a177af277d5262
Aimeos <aimeos@aimeos.org>
2021-10-07 09:07:17 +0200

    Improved address form

--- client/html/templates/checkout/standard/address-partial-standard.php
+++ client/html/templates/checkout/standard/address-partial-standard.php
@@ -11,9 +11,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 
 ?>
 <div class="row form-item form-group salutation <?= $enc->attr( ( $this->value( 'error', 'order.base.address.salutation' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.salutation', [] ) ) ) ?>">
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-salutation-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Salutation' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-salutation-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Salutation' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<select class="form-control" id="address-<?= $this->get( 'type', 'billing' ) ?>-salutation-<?= $this->get( 'id' ) ?>"
 			name="<?= $enc->attr( $this->formparam( array( $fname, 'order.base.address.salutation' ) ) ) ?>"
@@ -37,9 +39,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group firstname <?= $enc->attr( ( $this->value( 'error', 'order.base.address.firstname' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.firstname', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/firstname' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-firstname-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'First name' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-firstname-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'First name' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-firstname-<?= $this->get( 'id' ) ?>"
@@ -55,9 +59,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group lastname <?= $enc->attr( ( $this->value( 'error', 'order.base.address.lastname' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.lastname', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/lastname' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-lastname-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Last name' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-lastname-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Last name' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-lastname-<?= $this->get( 'id' ) ?>"
@@ -73,9 +79,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group company <?= $enc->attr( ( $this->value( 'error', 'order.base.address.company' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.company', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/company' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-company-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Company' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-company-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Company' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-company-<?= $this->get( 'id' ) ?>"
@@ -91,9 +99,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group address1 <?= $enc->attr( ( $this->value( 'error', 'order.base.address.address1' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.address1', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/address1' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-address1-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Street' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-address1-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Street' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-address1-<?= $this->get( 'id' ) ?>"
@@ -109,9 +119,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group address2 <?= $enc->attr( ( $this->value( 'error', 'order.base.address.address2' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.address2', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/address2' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-address2-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Additional' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-address2-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Additional' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-address2-<?= $this->get( 'id' ) ?>"
@@ -127,9 +139,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group address3 <?= $enc->attr( ( $this->value( 'error', 'order.base.address.address3' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.address3', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/address3' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-address3-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Additional 2' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-address3-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Additional 2' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-address3-<?= $this->get( 'id' ) ?>"
@@ -145,9 +159,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group city <?= $enc->attr( ( $this->value( 'error', 'order.base.address.city' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.city', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/city' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-city-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'City' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-city-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'City' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-city-<?= $this->get( 'id' ) ?>"
@@ -163,9 +179,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group postal <?= $enc->attr( ( $this->value( 'error', 'order.base.address.postal' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.postal', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/postal' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-postal-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Postal code' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-postal-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Postal code' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-postal-<?= $this->get( 'id' ) ?>"
@@ -181,9 +199,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <?php if( count( $this->get( 'states', [] ) ) > 0 ) : ?>
 	<div class="row form-item form-group state <?= $enc->attr( ( $this->value( 'error', 'order.base.address.state' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.state', [] ) ) ) ?>">
 
-		<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-state-<?= $this->get( 'id' ) ?>">
-			<?= $enc->html( $this->translate( 'client', 'State' ), $enc::TRUST ) ?>
-		</label>
+		<div class="col-md-5">
+			<label for="address-<?= $this->get( 'type', 'billing' ) ?>-state-<?= $this->get( 'id' ) ?>">
+				<?= $enc->html( $this->translate( 'client', 'State' ), $enc::TRUST ) ?>
+			</label>
+		</div>
 		<div class="col-md-7">
 			<select class="form-control" id="address-<?= $this->get( 'type', 'billing' ) ?>-state-<?= $this->get( 'id' ) ?>"
 				name="<?= $enc->attr( $this->formparam( array( $fname, 'order.base.address.state' ) ) ) ?>"
@@ -214,9 +234,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <?php if( count( $this->get( 'countries', [] ) ) > 0 ) : ?>
 	<div class="row form-item form-group countryid <?= $enc->attr( ( $this->value( 'error', 'order.base.address.countryid' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.countryid', [] ) ) ) ?>">
 
-		<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-countryid-<?= $this->get( 'id' ) ?>">
-			<?= $enc->html( $this->translate( 'client', 'Country' ), $enc::TRUST ) ?>
-		</label>
+		<div class="col-md-5">
+			<label for="address-<?= $this->get( 'type', 'billing' ) ?>-countryid-<?= $this->get( 'id' ) ?>">
+				<?= $enc->html( $this->translate( 'client', 'Country' ), $enc::TRUST ) ?>
+			</label>
+		</div>
 		<div class="col-md-7">
 			<select class="form-control" id="address-<?= $this->get( 'type', 'billing' ) ?>-countryid-<?= $this->get( 'id' ) ?>"
 				name="<?= $enc->attr( $this->formparam( array( $fname, 'order.base.address.countryid' ) ) ) ?>"
@@ -242,9 +264,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 	<div class="row form-item form-group languageid <?= $enc->attr( ( $this->value( 'error', 'order.base.address.languageid' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.languageid', [] ) ) ) ?>"
 		<?= $this->value( 'css', 'order.base.address.languageid' ) ? '' : 'disabled' ?>>
 
-		<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-languageid-<?= $this->get( 'id' ) ?>">
-			<?= $enc->html( $this->translate( 'client', 'Language' ), $enc::TRUST ) ?>
-		</label>
+		<div class="col-md-5">
+			<label for="address-<?= $this->get( 'type', 'billing' ) ?>-languageid-<?= $this->get( 'id' ) ?>">
+				<?= $enc->html( $this->translate( 'client', 'Language' ), $enc::TRUST ) ?>
+			</label>
+		</div>
 		<div class="col-md-7">
 			<select class="form-control" id="address-<?= $this->get( 'type', 'billing' ) ?>-languageid-<?= $this->get( 'id' ) ?>"
 				name="<?= $enc->attr( $this->formparam( array( $fname, 'order.base.address.languageid' ) ) ) ?>">
@@ -268,9 +292,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group vatid <?= $enc->attr( ( $this->value( 'error', 'order.base.address.vatid' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.vatid', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/vatid' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-vatid-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Vat ID' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-vatid-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Vat ID' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="text"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-vatid-<?= $this->get( 'id' ) ?>"
@@ -286,9 +312,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group email <?= $enc->attr( ( $this->value( 'error', 'order.base.address.email' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.email', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/email', '^.+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)*$' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-email-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'E-Mail' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-email-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'E-Mail' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="email"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-email-<?= $this->get( 'id' ) ?>"
@@ -304,9 +332,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group telephone <?= $enc->attr( ( $this->value( 'error', 'order.base.address.telephone' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.telephone', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/telephone' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-telephone-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Telephone' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-telephone-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Telephone' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="tel"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-telephone-<?= $this->get( 'id' ) ?>"
@@ -322,9 +352,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group telefax <?= $enc->attr( ( $this->value( 'error', 'order.base.address.telefax' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.telefax', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/telefax' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-telefax-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Fax' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-telefax-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Fax' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="tel"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-telefax-<?= $this->get( 'id' ) ?>"
@@ -340,9 +372,11 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 <div class="row form-item form-group website <?= $enc->attr( ( $this->value( 'error', 'order.base.address.website' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.website', [] ) ) ) ?>"
 	data-regex="<?= $enc->attr( $this->config( 'client/html/checkout/standard/address/validate/website', '^([a-z]+://)?[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)+(:[0-9]+)?(/.*)?$' ) ) ?>">
 
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-website-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Web site' ), $enc::TRUST ) ?>
-	</label>
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-website-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Web site' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="url"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-website-<?= $this->get( 'id' ) ?>"
@@ -356,9 +390,12 @@ $fname = 'ca_' . $this->get( 'type', 'billing' ) . ( $this->get( 'id' ) ? '_' : 
 
 
 <div class="row form-item form-group birthday <?= $enc->attr( ( $this->value( 'error', 'order.base.address.birthday' ) ? 'error ' : '' ) . join( ' ', $this->value( 'css', 'order.base.address.birthday', [] ) ) ) ?>">
-	<label class="col-md-5" for="address-<?= $this->get( 'type', 'billing' ) ?>-birthday-<?= $this->get( 'id' ) ?>">
-		<?= $enc->html( $this->translate( 'client', 'Birthday' ), $enc::TRUST ) ?>
-	</label>
+
+	<div class="col-md-5">
+		<label for="address-<?= $this->get( 'type', 'billing' ) ?>-birthday-<?= $this->get( 'id' ) ?>">
+			<?= $enc->html( $this->translate( 'client', 'Birthday' ), $enc::TRUST ) ?>
+		</label>
+	</div>
 	<div class="col-md-7">
 		<input class="form-control" type="date"
 			id="address-<?= $this->get( 'type', 'billing' ) ?>-birthday-<?= $this->get( 'id' ) ?>"
