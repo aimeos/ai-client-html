0ce2e1166f52c86e432bb774475e1caf4b8cb9fd
Aimeos <aimeos@aimeos.org>
2021-12-28 17:45:33 +0100

    Simplified catalog/count tree JS

--- client/html/templates/catalog/count/tree-body-standard.php
+++ client/html/templates/catalog/count/tree-body-standard.php
@@ -14,13 +14,11 @@ var catalogCounts = <?= $this->get( 'treeCountList', map() )->toJson( JSON_FORCE
 $(".catalog-filter-count .cat-item").each(function(index, item) {
 	var id = $(item).data("id");
 
-	$("a.cat-item", item).append(function() {
-		if( catalogCounts[id] ) {
-			return '<span class="cat-count">' + catalogCounts[id] + '</span>';
-		} else if($(item).hasClass("nochild")) {
-			$(item).addClass("disabled");
-		}
-	});
+	if(catalogCounts[id]) {
+		$("a.cat-item", item).append('<span class="cat-count">' + catalogCounts[id] + '</span>');
+	} else if($(item).hasClass("nochild")) {
+		$(item).addClass("disabled");
+	}
 });
 // -->
 <?php $this->block()->stop() ?>
