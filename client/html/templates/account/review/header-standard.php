c29fadf2e6636e33e30cec56fab62e5ff63a51f9
Aimeos <aimeos@aimeos.org>
2022-01-08 09:29:05 +0100

    Moved account review CSS/JS to own files

--- client/html/templates/account/review/header-standard.php
+++ client/html/templates/account/review/header-standard.php
@@ -5,5 +5,11 @@
  * @copyright Aimeos (aimeos.org), 2020-2022
  */
 
+$enc = $this->encoder();
+
+
 ?>
-<?= $this->get( 'reviewHeader' );
+<link rel="stylesheet" href="<?= $enc->attr( $this->content( $this->get( 'contextSite', 'default' ) . '/account-review.css', 'fs-theme' ) ) ?>">
+<script defer src="<?= $enc->attr( $this->content( $this->get( 'contextSite', 'default' ) . '/account-review.js', 'fs-theme' ) ) ?>"></script>
+
+<?= $this->get( 'reviewHeader' ) ?>
