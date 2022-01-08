428902e0595721b1b5e28cd0e525586fe13e4250
Aimeos <aimeos@aimeos.org>
2022-01-08 09:28:34 +0100

    Moved account history CSS/JS to own files

--- client/html/templates/account/history/header-standard.php
+++ client/html/templates/account/history/header-standard.php
@@ -2,9 +2,14 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2013
  * @copyright Aimeos (aimeos.org), 2015-2022
  */
 
+$enc = $this->encoder();
+
+
 ?>
-<?= $this->get( 'historyHeader' );
+<link rel="stylesheet" href="<?= $enc->attr( $this->content( $this->get( 'contextSite', 'default' ) . '/account-history.css', 'fs-theme' ) ) ?>">
+<script defer src="<?= $enc->attr( $this->content( $this->get( 'contextSite', 'default' ) . '/account-history.js', 'fs-theme' ) ) ?>"></script>
+
+<?= $this->get( 'historyHeader' ) ?>
--- client/html/themes/default/aimeos.js
+++ client/html/themes/default/aimeos.js
@@ -192,77 +192,6 @@ Aimeos = {
 
 
 
-
-
-
-
-
-
-/**
- * Account history actions
- */
-AimeosAccountHistory = {
-
-	/**
-	 * Shows order details without page reload
-	 */
-	setupOrderShow: function() {
-
-		$(".account-history .history-item").on("click", '.action .btn', function() {
-
-			var target = $(this).parents(".history-item");
-			var details = $(".account-history-order", target);
-
-			if(details.length === 0) {
-
-				fetch($(this).attr("href")).then(response => {
-					return response.text();
-				}).then(data => {
-					var doc = document.createElement("html");
-					doc.innerHTML = data;
-
-					var node = $(".account-history-order", doc);
-					node.css("display", "none");
-					target.append(node);
-					slideDown(node.get(0), 300);
-				});
-
-			} else {
-				slideToggle(details.get(0), 300);
-			}
-
-			return false;
-		});
-	},
-
-
-	/**
-	 * Closes the order details without page reload
-	 */
-	setupOrderClose: function() {
-
-		$(".account-history .history-item").on("click", ".btn-close", function() {
-			$(".account-history-order", $(this).parents(".history-item")).each(function() {
-				slideUp(this, 300);
-			});
-			return false;
-		});
-	},
-
-
-	/**
-	 * Initializes the account history actions
-	 */
-	init: function() {
-
-		this.setupOrderShow();
-		this.setupOrderClose();
-	}
-};
-
-
-
-
 /**
  * Account profile actions
  */
@@ -1122,7 +1051,6 @@ $(function() {
 
 	AimeosAccountProfile.init();
 	AimeosAccountSubscription.init();
-	AimeosAccountHistory.init();
 	AimeosAccountWatch.init();
 
 	Aimeos.loadImages();
