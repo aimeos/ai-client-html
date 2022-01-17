2259b7347c8f356bb64dac3af2ac2b90cb246087
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2022-01-17 10:17:53 +0100

    Scrutinizer Auto-Fixes (#166)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- client/html/templates/catalog/lists/items-body-list.php
+++ client/html/templates/catalog/lists/items-body-list.php
@@ -32,7 +32,7 @@ if( $infiniteScroll && $this->get( 'listPageNext', 0 ) > $this->get( 'listPageCu
 ?>
 <?php $this->block()->start( 'catalog/lists/items' ) ?>
 <div class="catalog-list-items list" data-infiniteurl="<?= $url ?>"
-	data-pinned="<?= $enc->attr( $this->session( 'aimeos/catalog/session/pinned/list', [] )) ?>"><!--
+	data-pinned="<?= $enc->attr( $this->session( 'aimeos/catalog/session/pinned/list', [] ) ) ?>"><!--
 
 	<?php foreach( $this->get( 'listProductItems', [] ) as $id => $productItem ) : $firstImage = true ?>
 		<?php
