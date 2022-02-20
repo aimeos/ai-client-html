0fb12bb9ffbf622db7d3ff6de516e3c261641d8f
Aimeos <aimeos@aimeos.org>
2022-02-20 12:37:55 +0100

    Improved account e-mail job controller

--- controller/jobs/src/Controller/Jobs/Customer/Email/Account/Standard.php
+++ controller/jobs/src/Controller/Jobs/Customer/Email/Account/Standard.php
@@ -69,8 +69,13 @@ class Standard
 
 				$password = $list['customer.password'] ?? null;
 				$item = $custManager->create()->fromArray( $list, true );
+				$sites = $this->sites( $item->getSiteId() );
 
-				$this->send( $item, $password );
+				$view = $this->view( $item->getPaymentAddress(), $sites->getTheme()->filter()->last() );
+				$view->account = $item->getCode();
+				$view->password = $password;
+
+				$this->send( $view, $item->getPaymentAddress(), $sites->getLogo()->filter()->last() );
 
 				$str = sprintf( 'Sent customer account e-mail to "%1$s"', $item->getPaymentAddress()->getEmail() );
 				$context->logger()->debug( $str, 'email/customer/account' );
@@ -89,25 +94,67 @@ class Standard
 	/**
 	 * Sends the account creation e-mail to the e-mail address of the customer
 	 *
-	 * @param \Aimeos\MShop\Customer\Item\Iface $item Customer item object
-	 * @param string|null $password Customer clear text password
+	 * @param \Aimeos\MW\View\Iface $view View object
+	 * @param \Aimeos\MShop\Common\Item\Address\Iface $address Address item
+	 * @param string|null $logoPath Path to the logo
 	 */
-	protected function send( \Aimeos\MShop\Customer\Item\Iface $item, string $password = null )
+	protected function send( \Aimeos\MW\View\Iface $view, \Aimeos\MShop\Common\Item\Address\Iface $address, string $logoPath = null )
 	{
 		$context = $this->context();
 		$config = $context->config();
-		$address = $item->getPaymentAddress();
 
-		$view = $this->call( 'mailView', $address->getLanguageId() );
-		$view->intro = $this->call( 'mailIntro', $address );
-		$view->account = $item->getCode();
-		$view->password = $password;
-		$view->addressItem = $address;
+		$msg = $this->call( 'mailTo', $address );
+		$view->logo = $msg->embed( $this->call( 'mailLogo', $logoPath ), basename( (string) $logoPath ) );
 
-		$this->call( 'mailTo', $address )
-			->subject( $context->translate( 'client', 'Your new account' ) )
+		$msg->subject( $context->translate( 'client', 'Your new account' ) )
 			->html( $view->render( $config->get( 'controller/jobs/customer/email/account/template-html', 'customer/email/account/html' ) ) )
 			->text( $view->render( $config->get( 'controller/jobs/customer/email/account/template-text', 'customer/email/account/text' ) ) )
 			->send();
+	}
+
+
+	/**
+	 * Returns the list of site items from the given site ID up to the root site
+	 *
+	 * @param string|null $siteId Site ID like "1.2.4."
+	 * @return \Aimeos\Map List of site items
+	 */
+	protected function sites( string $siteId = null ) : \Aimeos\Map
+	{
+		if( !$siteId && !isset( $this->sites[''] ) ) {
+			$this->sites[''] = map( \Aimeos\MShop::create( $this->context(), 'locale/site' )->find( 'default' ) );
+		}
+
+		if( !isset( $this->sites[(string) $siteId] ) )
+		{
+			$manager = \Aimeos\MShop::create( $this->context(), 'locale/site' );
+			$siteIds = explode( '.', trim( $siteId, '.' ) );
+
+			$this->sites[$siteId] = $manager->getPath( end( $siteIds ) );
+		}
+
+		return $this->sites[$siteId];
+	}
+
+
+	/**
+	 * Returns the view populated with common data
+	 *
+	 * @param \Aimeos\MShop\Common\Item\Address\Iface $address Address item
+	 * @param string|null $theme Theme name
+	 * @return \Aimeos\MW\View\Iface View object
+	 */
+	protected function view( \Aimeos\MShop\Common\Item\Address\Iface $address, string $theme = null ) : \Aimeos\MW\View\Iface
+	{
+		$view = $this->call( 'mailView', $address->getLanguageId() );
+		$view->intro = $this->call( 'mailIntro', $address );
+		$view->css = $this->call( 'mailCss', $theme );
+		$view->addressItem = $address;
+		$view->urlparams = [
+			'site' => $this->context()->locale()->getSiteItem()->getCode(),
+			'locale' => $address->getLanguageId(),
+		];
+
+		return $view;
 	}
 }
