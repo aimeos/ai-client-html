01097cf1acc4193d13bffe61f0893c3770208f36
Aimeos <aimeos@aimeos.org>
2022-05-08 16:35:39 +0200

    Use <a/> tag for hamburger menu in catalog filter tree

--- templates/client/html/catalog/filter/tree-body.php
+++ templates/client/html/catalog/filter/tree-body.php
@@ -43,7 +43,7 @@ $enforce = $this->config( 'client/html/catalog/filter/tree/force-search', false 
 	<section class="catalog-filter-tree <?= ( $this->config( 'client/html/catalog/count/enable', true ) ? 'catalog-filter-count' : '' ); ?>">
 
 		<div class="aimeos-overlay-offscreen"></div>
-		<div class="menu"></div>
+		<a class="menu" href="#" title="<?= $enc->attr( $this->translate( 'client', 'Categories' ) ) ?>"><span class="icon"></span></a>
 		<div class="zeynep list-container level-0 catcode-<?= $enc->attr( $this->treeCatalogTree->getCode() ) ?> <?= $enc->attr( $this->treeCatalogTree->getConfigValue( 'css-class' ) ) ?>">
 
 			<div class="row header">
