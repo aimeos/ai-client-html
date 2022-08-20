73564e405151b8b4c4cd7df5aa37ef49150362a9
Aimeos <aimeos@aimeos.org>
2022-08-20 13:22:42 +0200

    Fixed URL and position of JSON breadbrumb

--- templates/client/html/catalog/stage/body.php
+++ templates/client/html/catalog/stage/body.php
@@ -30,13 +30,13 @@ $enc = $this->encoder();
 	<div class="catalog-stage-breadcrumb container-xxl">
 		<?php if( isset( $this->stageCatPath ) ) : ?>
 			<?php
-				$entries = []; $idx = 1;
+				$entries = []; $idx = 0;
 				foreach( $this->get( 'stageCatPath', map() ) as $cat )
 				{
 					$entries['@type'] = 'ListItem';
-					$entries['position'] = $idx++;
+					$entries['position'] = ++$idx;
 					$entries['name'] = $cat->getName();
-					$entries['item'] = $this->link( 'client/html/catalog/tree/url', array_merge( $this->get( 'stageParams', [] ), ['f_name' => $cat->getName( 'url' ), 'f_catid' => $cat->getId()] ) );
+					$entries['item'] = $this->link( 'client/html/catalog/tree/url', ['f_name' => $cat->getName( 'url' ), 'f_catid' => $cat->getId()] );
 				}
 			?>
 			<script type="application/ld+json">
