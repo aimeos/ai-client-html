dc3002789fad17b607261b992e26cc41df3a14d9
Aimeos <aimeos@aimeos.org>
2022-12-15 10:07:36 +0100

    Keep passed f_catid even if start category is configured for catalog count URL

--- src/Client/Html/Catalog/Filter/Standard.php
+++ src/Client/Html/Catalog/Filter/Standard.php
@@ -516,7 +516,8 @@ class Standard
 			 */
 
 			if( $startid = $config->get( 'client/html/catalog/filter/tree/startid' ) ) {
-				$params['f_catid'] = $startid;
+				$params['f_catid'] = $this->param( 'f_catid', $startid );
+				$params['f_name'] = $this->param( 'f_name', '' );
 			}
 
 			foreach( $config->get( 'client/html/catalog/filter/remove-params', ['f_sort'] ) as $name ) {
