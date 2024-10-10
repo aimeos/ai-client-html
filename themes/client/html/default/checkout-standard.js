c31937b5d2cc5b6e56040c1861de5c8e7172b008
Aimeos <aimeos@aimeos.org>
2024-10-10 16:39:03 +0200

    Always disable address fields if they are not mandatory or optional

--- themes/client/html/default/checkout-standard.js
+++ themes/client/html/default/checkout-standard.js
@@ -10,13 +10,13 @@ AimeosCheckoutStandard = {
 
 		document.querySelectorAll(".checkout-standard-address .item-address").forEach(el => {
 			el.addEventListener("show.bs.collapse", ev => {
-				$(".form-list input,select", ev.currentTarget).prop("disabled", false);
+				$(".form-item.mandatory input, .form-item.mandatory select, .form-item.optional input, .form-item.optional select", ev.currentTarget).prop("disabled", false);
 			});
 		});
 
 		document.querySelectorAll(".checkout-standard-address .item-address").forEach(el => {
 			el.addEventListener("hidden.bs.collapse", ev => {
-				$(".form-list input,select", ev.currentTarget).prop("disabled", true);
+				$(".form-item input, .form-item select", ev.currentTarget).prop("disabled", true);
 			});
 		});
 	},
