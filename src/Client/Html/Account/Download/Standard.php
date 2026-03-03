b78233958aecf3bba83981aa04a893ca79b61e1f
Aimeos <aimeos@aimeos.org>
2026-03-03 08:44:40 +0100

    Fixed download counter

--- src/Client/Html/Account/Download/Standard.php
+++ src/Client/Html/Account/Download/Standard.php
@@ -148,7 +148,7 @@ class Standard
 
 			$response->withHeader( 'Content-Description', 'File Transfer' );
 			$response->withHeader( 'Content-Type', 'application/octet-stream' );
-			$response->withHeader( 'Content-Disposition', 'attachment; filename="' . $name . '"' );
+			$response->withHeader( 'Content-Disposition', 'attachment; filename="' . str_replace( ["\r", "\n", '"'], '', $name ) . '"' );
 			$response->withHeader( 'Content-Length', (string) $fs->size( $value ) );
 			$response->withHeader( 'Cache-Control', 'must-revalidate' );
 			$response->withHeader( 'Pragma', 'private' );
@@ -178,7 +178,7 @@ class Standard
 	{
 		$context = $this->context();
 
-		if( $id && ( $customerId = $context->user() ) )
+		if( $id && ( $customerId = (string) $context->user() ) )
 		{
 			$manager = \Aimeos\MShop::create( $context, 'order' );
 
@@ -235,7 +235,7 @@ class Standard
 
 		if( $maxcnt === null || $count < $maxcnt )
 		{
-			$config['count'] = $count++;
+			$config['count'] = ++$count;
 			$cntl->addListItem( 'order', $listItem->setConfig( $config ) )->store();
 
 			return true;
